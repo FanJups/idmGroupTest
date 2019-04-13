05a7438729f13ed46ab07bcb8141fd9613918df8
FanJups <jupsfan@gmail.com>
2019-04-13 15:48:11 +0200

    TimeDuration

---
+++ TimeDuration/src/main/java/com/timeduration/exception/BadBadValueException.java
@@ -0,0 +1,9 @@
+package com.timeduration.exception;
+
+public class BadBadValueException extends Exception {
+
+	public BadBadValueException(String errorMessage) {
+		super(errorMessage);
+	}
+
+}
---
+++ TimeDuration/src/test/java/com/timeduration/model/TestTimeDuration.java
@@ -0,0 +1,39 @@
+package com.timeduration.model;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.timeduration.exception.BadBadValueException;
+
+public class TestTimeDuration {
+
+	@Test
+	public void testToString0() throws BadBadValueException {
+
+		assertEquals("0s", new TimeDuration(0).toString());
+
+	}
+
+	@Test
+	public void testToString732() throws BadBadValueException {
+
+		assertEquals("12m 12s", new TimeDuration(732).toString());
+
+	}
+
+	@Test
+	public void testToString7242() throws BadBadValueException {
+
+		assertEquals("2h 0m 42s", new TimeDuration(7242).toString());
+
+	}
+
+	@Test(expected = BadBadValueException.class)
+	public void testToStringNegative() throws BadBadValueException {
+
+		System.out.println(new TimeDuration(-2).toString());
+
+	}
+
+}
