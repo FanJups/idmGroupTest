9235b2ab3a02b5ba768f4f4d8ddcfb93c099c46d
FanJups <jupsfan@gmail.com>
2019-04-17 00:16:17 +0200

    RaceResults

---
+++ RaceResults/src/main/java/com/raceresults/exception/BadBadValueException.java
@@ -0,0 +1,9 @@
+package com.raceresults.exception;
+
+public class BadBadValueException extends Exception {
+
+	public BadBadValueException(String errorMessage) {
+		super(errorMessage);
+	}
+
+}
---
+++ RaceResults/src/main/java/com/raceresults/model/TimeDuration.java
@@ -0,0 +1,64 @@
+package com.raceresults.model;
+
+import com.raceresults.exception.BadBadValueException;
+
+public class TimeDuration {
+
+	protected int seconds;
+
+	public TimeDuration(int secondsInput) throws BadBadValueException {
+
+		try {
+
+			if (secondsInput < 0)
+				throw new BadBadValueException("Incorrect number because it's negative");
+
+			this.seconds = secondsInput;
+
+		} catch (BadBadValueException e) {
+
+			throw e;
+
+		}
+	}
+
+	public String toString() {
+
+		String result = "";
+
+		int sec = seconds % 60;
+		int h = seconds / 60;
+		int min = h % 60;
+		h = h / 60;
+
+		if (h == 0) {
+
+			if (min == 0) {
+
+				result = sec + "s";
+
+			} else {
+
+				result = min + "m " + sec + "s";
+
+			}
+
+		} else {
+
+			result = h + "h " + min + "m " + sec + "s";
+
+		}
+
+		return result;
+
+	}
+
+	public int getSeconds() {
+		return seconds;
+	}
+
+	public void setSeconds(int seconds) {
+		this.seconds = seconds;
+	}
+
+}
